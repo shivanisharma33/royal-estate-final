7e5f5b18d291e87389b0138252054b1828f042e2
shividixit <shivani.midis@gmail.com>
2026-01-08 17:01:59 +0530

    indicator

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -93,25 +93,6 @@ export const Hero = () => {
         </motion.div>
       </div>
 
-      {/* SCROLL INDICATOR */}
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ delay: 1.5 }}
-        className="absolute bottom-8 left-1/2 -translate-x-1/2 z-10"
-      >
-        <motion.div
-          animate={{ y: [0, 10, 0] }}
-          transition={{ repeat: Infinity, duration: 2 }}
-          className="w-6 h-10 border-2 border-primary/50 rounded-full flex justify-center pt-2"
-        >
-          <motion.div
-            animate={{ opacity: [0.5, 1, 0.5] }}
-            transition={{ repeat: Infinity, duration: 2 }}
-            className="w-1.5 h-1.5 bg-primary rounded-full"
-          />
-        </motion.div>
-      </motion.div>
 
       {/* CONSULTATION POPUP */}
       <AnimatePresence>
